7e1afc863dfaa106197002c7a16194ec83f9dd91
neuricelio <neuricelio@gmail.com>
2026-08-01 03:34:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ app.listen(porta, () => {
 // CONEXÃO COM MYSQL
 // ------------------------------
 const bd = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'sua-senha-local',
-  database: process.env.DB_NAME || 'sistema_cadastro',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
