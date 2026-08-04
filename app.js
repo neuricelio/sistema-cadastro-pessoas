d0cea7f286ca81cf9ab0938e5578e8fb53bf0185
neuricelio <neuricelio@gmail.com>
2026-08-04 02:21:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -77,7 +77,8 @@ app.use(session({
 // ------------------------------
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/fotos', express.static(caminhoPasta));
 app.set('view engine', 'ejs');
 
